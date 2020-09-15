3dcdb71d37faaf4f1db49b36a6685be4dbc76a00
DESKTOP-GIC1I9I\HORAD <gutsueric@gmail.com>
2020-09-16 00:53:48 +0300

    test(MainTest) dont look here :)

--- Lab1/src/main/java/MainTest.java
+++ Lab1/src/main/java/MainTest.java
@@ -1,13 +1,24 @@
+import common.BrokerParameters;
+import common.JsonParser;
 import org.json.JSONObject;
+import common.JsonSerializer;
+import subscriber.SubscriberSocket;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class MainTest {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        JSONObject str = JsonSerializer
-                .deserializeJSON(JsonSerializer
-                        .serializeJSON(JsonSerializer
-                                .getJSONByData(1, "tp1", "hello world")));
+        JSONObject str = JsonSerializer.getJSONByData("1", "topic1", "kekw");
         System.out.println(str);
+        byte[] strByte = JsonSerializer.serializeJSON(str);
+        System.out.println(Arrays.toString(strByte));
+        JSONObject strDeserialized = JsonSerializer.deserializeJSON(strByte);
+        System.out.println(strDeserialized);
+        JsonParser parser = new JsonParser();
+        parser.parseJSON(strDeserialized);
+        System.out.println("id " + parser.getId() + " topic " + parser.getTopic() + " message " + parser.getMessage());
+
+//        SubscriberSocket subscriberSocket = new SubscriberSocket(BrokerParameters.BROKER_IP, BrokerParameters.BROKER_PORT);
     }
 }
