bbf742b6362692c3c79f678dea251dc92463fdbf
DESKTOP-GIC1I9I\HORAD <gutsueric@gmail.com>
2020-09-16 00:46:42 +0300

    remake(Publisher) add publisher which connects to the Broker with PublisherSocket

--- Lab1/src/main/java/publisher/Publisher.java
+++ Lab1/src/main/java/publisher/Publisher.java
@@ -1,8 +1,31 @@
 package publisher;
 
+import common.BrokerParameters;
+import common.JsonSerializer;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Publisher {
+    public static Scanner sc = new Scanner(System.in);
+    static int messageCount = 0;
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Publisher");
+        PublisherSocket publisherSocket = new PublisherSocket();
+        publisherSocket.connect(BrokerParameters.BROKER_IP, BrokerParameters.BROKER_PORT);
+        if (publisherSocket.isConnected) {
+            while (true) {
+                messageCount++;
+                System.out.println("Enter Topic");
+                String topic = sc.nextLine().toLowerCase();
+                System.out.println("Enter Message");
+                String message = sc.nextLine();
+                JSONObject data = JsonSerializer.getJSONByData(Integer.toString(messageCount), topic, message);
+                byte[] serializedData = JsonSerializer.serializeJSON(data);
+                publisherSocket.send(serializedData);
+            }
         }
     }
 }
