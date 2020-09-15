21ac7669ee94d0d89c095ad8cad9b172427b912e
DESKTOP-GIC1I9I\HORAD <gutsueric@gmail.com>
2020-09-15 11:43:21 +0300

    model() make model for visualizing communication between Server <--> Publisher (delete in future expansion of project)

---
+++ Lab1/src/main/java/publisher/PublisherSocket.java
@@ -0,0 +1,15 @@
+package publisher;
+
+import java.net.Socket;
+
+public class PublisherSocket {
+    private Socket socket;
+
+    public PublisherSocket() {
+        this.socket = new Socket();
+    }
+
+    public void Connect(String ipAddress, int port) {
+    }
+
+}
