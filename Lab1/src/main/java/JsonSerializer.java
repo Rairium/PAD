664da79e96e62c659d4bb1d5626b8bedaeb0f4e4
DESKTOP-GIC1I9I\HORAD <gutsueric@gmail.com>
2020-09-15 11:42:04 +0300

    feat(JsonSerializer) add Json Serializer for future using on sending data between sockets

---
+++ Lab1/src/main/java/JsonSerializer.java
@@ -0,0 +1,32 @@
+
+import org.json.JSONObject;
+
+import java.io.*;
+
+public class JsonSerializer implements Serializable {
+
+    public static JSONObject getJSONByData(Integer id, String topic, String message) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", id);
+        jsonObject.put("topic", topic);
+        jsonObject.put("message", message);
+        return jsonObject;
+    }
+
+    public static byte[] serializeJSON(JSONObject jsonObject) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(jsonObject.toString());
+        objectOutputStream.flush();
+        return byteArrayOutputStream.toByteArray    ();
+
+    }
+
+    public static JSONObject deserializeJSON(byte[] data) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+        ObjectInput input = new ObjectInputStream(byteArrayInputStream);
+
+        String deserializedData = (String) input.readObject();
+        return new JSONObject(deserializedData);
+    }
+}
